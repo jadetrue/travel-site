dff1ef237177a073a45fcc51d7e462a3f7904192
Jade <jadetruexo@gmail.com>
2020-05-28 13:20:14 +0100

    JavaScript intro bits

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,11 @@ import '../styles/styles.css';
 if (module.hot) {
     module.hot.accept();
 }
+
+/* Lesson example code below this line */
+
+var john = new Person("John Doe", "Blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "Green");
+jane.greet();
