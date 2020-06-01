c483c5bd92437ee9e29893d3da53c408aa06276f
Jade True <jadetruexo@gmail.com>
2020-06-01 14:26:19 +0100

    Completed the reveal on the scroll behaviour.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,9 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 
-let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
