2843b26f6c094111f01b609a804dadf1a3c38564
Jade <jadetruexo@gmail.com>
2020-06-02 12:50:01 +0100

    Completed functionality of modal.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,9 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
+
+new Modal();
 
 let stickyHeader = new StickyHeader();
 
