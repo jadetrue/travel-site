e624c28298c0a0a11d2e66160f516bacd71b5a00
Jade <jadetruexo@gmail.com>
2020-06-04 11:26:18 +0100

    Updated secret area file

--- app/cloud-functions/secret-area.js
+++ app/cloud-functions/secret-area.js
@@ -1,4 +1,4 @@
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
     callback(null, {
         statusCode: 200,
         body: "Welcome to the super secret area"
