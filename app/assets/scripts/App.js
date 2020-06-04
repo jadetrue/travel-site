70e9c108f3072fcceded590d19d3ecec406c428a
Jade <jadetruexo@gmail.com>
2020-06-04 10:11:16 +0100

    Completed build process.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,6 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
