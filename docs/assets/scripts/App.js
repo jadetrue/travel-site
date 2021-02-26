3c20a9e3c38acba66540d222615f0c820f5427a6
Jade True <jadetruexo@gmail.com>
2021-02-26 20:36:49 +0000

    updated git ignore file

---
+++ docs/assets/scripts/App.js
@@ -0,0 +1,35 @@
+import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+// React related code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Import React components
+import MyAmazingComponent from "./modules/MyAmazingComponent";
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector('#my-react-example'));
+
+new StickyHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        if(typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log("There was a problem."));
+        } else {
+            modal.openTheModal();
+        }
+    })
+})
+
+if (module.hot) {
+    module.hot.accept();
+}
